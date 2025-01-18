c841e2b319649629db8313218c13aa3598d83750
hotwellkz <hotwell.kz@gmail.com>
2025-01-18 22:27:24 +0600

    Update registry.ts

--- app/lib/modules/llm/registry.ts
+++ app/lib/modules/llm/registry.ts
@@ -1,4 +1,6 @@
+import OpenAIProvider from './providers/openai';
 import AnthropicProvider from './providers/anthropic';
+import HyperbolicProvider from './providers/hyperbolic';
 import CohereProvider from './providers/cohere';
 import DeepseekProvider from './providers/deepseek';
 import GoogleProvider from './providers/google';
@@ -9,24 +11,22 @@ import MistralProvider from './providers/mistral';
 import OllamaProvider from './providers/ollama';
 import OpenRouterProvider from './providers/open-router';
 import OpenAILikeProvider from './providers/openai-like';
-import OpenAIProvider from './providers/openai';
 import PerplexityProvider from './providers/perplexity';
 import TogetherProvider from './providers/together';
 import XAIProvider from './providers/xai';
-import HyperbolicProvider from './providers/hyperbolic';
 import AmazonBedrockProvider from './providers/amazon-bedrock';
 
 export {
+  OpenAIProvider,
   AnthropicProvider,
+  HyperbolicProvider,
   CohereProvider,
   DeepseekProvider,
   GoogleProvider,
   GroqProvider,
   HuggingFaceProvider,
-  HyperbolicProvider,
   MistralProvider,
   OllamaProvider,
-  OpenAIProvider,
   OpenRouterProvider,
   OpenAILikeProvider,
   PerplexityProvider,
